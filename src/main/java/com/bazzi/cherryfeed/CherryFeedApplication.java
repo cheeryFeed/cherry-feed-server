57629539925b4f44f41cc13461e7b6c73514f2c8
Luca Kwak <cldud03@naver.com>
2023-03-07 17:06:45 +0900

    init

---
+++ src/main/java/com/bazzi/cherryfeed/CherryFeedApplication.java
@@ -0,0 +1,13 @@
+package com.bazzi.cherryfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryFeedApplication.class, args);
+	}
+
+}
